f0af94fb30ea65b4f6d949f3f21c82ef8ab3dcf4
Fabio Arcidiacono <fa.arcidiacono@gmail.com>
2013-01-10 10:59:09 +0100

    sistemato bug in abilitySuggestion

--- SWIMweb/src/it/polimi/swim/servlet/AbilitySuggestionServlet.java
+++ SWIMweb/src/it/polimi/swim/servlet/AbilitySuggestionServlet.java
@@ -34,9 +34,34 @@ public class AbilitySuggestionServlet extends HttpServlet {
 		// Not to implement, redirect
 		System.out
 				.println("*** [AbilitySuggestionServlet] doGet, forwarding to abilitySuggestion.jsp ***");
+		reloadUser(request, response);
 		getServletConfig().getServletContext()
 				.getRequestDispatcher("/user/abilitySuggestion.jsp")
 				.forward(request, response);
+	}
+
+	private void reloadUser(HttpServletRequest request,
+			HttpServletResponse response) {
+		try {
+			Hashtable<String, String> env = new Hashtable<String, String>();
+			env.put(Context.INITIAL_CONTEXT_FACTORY,
+					"org.jnp.interfaces.NamingContextFactory");
+			env.put(Context.PROVIDER_URL, "localhost:1099");
+			InitialContext jndiContext = new InitialContext(env);
+			Object ref = jndiContext.lookup("ProfileManager/remote");
+			ProfileManagerRemote profileManager = (ProfileManagerRemote) ref;
+			
+			User user = (User) request.getSession().getAttribute("User");
+
+			user = profileManager.reloadUser(user);
+			request.getSession().removeAttribute("User");
+			request.getSession().setAttribute("User", user);
+			
+		} catch (UserException ue) {
+			request.setAttribute("error", ue.getMessage());
+		} catch (NamingException e) {
+			request.setAttribute("error", "can't reach the server");
+		}
 	}
 
 	/**
@@ -45,51 +70,47 @@ public class AbilitySuggestionServlet extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request,
 			HttpServletResponse response) throws ServletException, IOException {
+
 		if (request.getParameter("suggestion") != null
 				&& request.getParameter("suggestion") != "") {
 			try {
+
 				Hashtable<String, String> env = new Hashtable<String, String>();
 				env.put(Context.INITIAL_CONTEXT_FACTORY,
 						"org.jnp.interfaces.NamingContextFactory");
 				env.put(Context.PROVIDER_URL, "localhost:1099");
 				InitialContext jndiContext = new InitialContext(env);
+				User user = (User) request.getSession().getAttribute("User");
+
 				Object ref = jndiContext.lookup("AbilityManager/remote");
 				AbilityManagerRemote abilityManager = (AbilityManagerRemote) ref;
 
-				User user = (User) request.getSession().getAttribute("User");
-
 				abilityManager.insertSuggestion(user,
 						request.getParameter("suggestion"));
-
-				ref = jndiContext.lookup("ProfileManager/remote");
-				ProfileManagerRemote profileManager = (ProfileManagerRemote) ref;
-				user = profileManager.reloadUser(user);
-				request.getSession().removeAttribute("User");
-				request.getSession().setAttribute("User", user);
 				System.out
 						.println("*** [AbilitySuggestionServlet] suggestion '"
 								+ request.getParameter("suggestion")
 								+ "' added ***");
 				request.setAttribute("message",
 						"your suggestion has been recorded");
+
 			} catch (NamingException e) {
 				request.setAttribute("error", "can't reach the server");
 			} catch (AbilityException asex) {
 				request.setAttribute("error", asex.getMessage());
-			} catch (UserException ue) {
-				request.setAttribute("error", ue.getMessage());
 			}
-			getServletConfig().getServletContext()
-					.getRequestDispatcher("/user/abilitySuggestion.jsp")
-					.forward(request, response);
 		} else {
 			System.out
 					.println("*** [AbilitySuggestionServlet] no suggestion, forwarding to abilitySuggestion.jsp ***");
 			request.setAttribute("error", "suggestion can not be empty");
-			getServletConfig().getServletContext()
-					.getRequestDispatcher("/user/abilitySuggestion.jsp")
-					.forward(request, response);
 		}
+
+		reloadUser(request, response);
+
+		getServletConfig().getServletContext()
+				.getRequestDispatcher("/user/abilitySuggestion.jsp")
+				.forward(request, response);
+
 	}
 
 }
